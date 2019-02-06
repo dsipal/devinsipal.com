b7fe7830b831116050e3ad168298c604831b88c3
devin <dsipal@unca.edu>
2019-02-06 15:42:28 -0500

    fixed posts loading multiple times

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	$(window).scroll(function() {
-	   if( (($(".post").last().offset().top + $(".post").last().height()) >= $(window).height()) &&
+	   if(
+		   (($(".post").last().offset().top + $(".post").last().height())
+		   >= $(window).height()) &&
 	   		!($('.posts').attr("data-totalpages") == $('.posts').attr("data-page") )
 		){
-		   loadMorePosts();
-
+		   loadMorePosts()
 	   }
 	})
 });
@@ -16,10 +17,10 @@ function loadMorePosts() {
   var nextPage = parseInt($container.attr("data-page")) + 1;
   var totalPages = parseInt($container.attr("data-totalpages"));
 
+  $container.attr("data-page", nextPage);
   $.get("/page/" + nextPage, function (data) {
     var htmlData = $.parseHTML(data);
     var $posts = $(htmlData).find(".post-container").hide();
-    $container.attr("data-page", nextPage).append($posts.fadeIn(1000));
-
+    $container.append($posts.fadeIn(1000));
   });
 }
