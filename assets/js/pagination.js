0fa0585c7b987639e9d9735bb114cfac49b8e385
devin <dsipal@unca.edu>
2019-02-06 12:53:32 -0500

    changes to pagination script

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -12,15 +12,11 @@ function loadMorePosts() {
   var $container = $(".posts");
   var nextPage = parseInt($container.attr("data-page")) + 1;
   var totalPages = parseInt($container.attr("data-totalpages"));
-  $(this).addClass("loading");
 
   $.get("/page/" + nextPage, function (data) {
     var htmlData = $.parseHTML(data);
     var $posts = $(htmlData).find(".post-container");
     $container.attr("data-page", nextPage).append($posts);
-    if ($container.attr("data-totalPages") == nextPage) {
-      $(".loadMore").remove();
-    }
-    $(_this).removeClass("loading");
+
   });
 }
