aebc8ffadab6b664240c4178872a7e07e659a2fe
devin <dsipal@unca.edu>
2019-02-08 23:43:22 -0500

    added parralax scrolling to main page, organized css, will continue organizing and developing the main page in here

---
+++ assets/js/home-parallax.js
@@ -0,0 +1,16 @@
+var left = new Rellax('#left-art', {
+	speed: 6,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+});
+var right = new Rellax('#right-art', {
+	speed: -6,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: true
+});
--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,25 +1,26 @@
-$(document).ready(function(){
-	$(window).scroll(function() {
-	   if(
-		   ( $(document).height() - $(window).height() - $(window).scrollTop()) < 70 &&
-	   		!($('.posts').attr("data-totalpages") == $('.posts').attr("data-page") )
-		){
-		   loadMorePosts()
-	   }
-	})
+$(document).ready(function() {
+	console.log($('feed-row').attr('data-page'));
+    $(window).scroll(function() {
+        if ($('body').height() <= $(window).height() ||
+            ($(document).height() - $(window).height() - $(window).scrollTop()) < 200 &&
+			$('.feed').attr('data-page') < $('.feed').attr('data-totalPages')
+		) {
+            loadMorePosts()
+        }
+    })
 });
 
 
 function loadMorePosts() {
-  var _this = this;
-  var $container = $(".posts");
-  var nextPage = parseInt($container.attr("data-page")) + 1;
-  var totalPages = parseInt($container.attr("data-totalpages"));
 
-  $container.attr("data-page", nextPage);
-  $.get("/page/" + nextPage, function (data) {
-    var htmlData = $.parseHTML(data);
-    var $posts = $(htmlData).find(".post-container").hide();
-    $container.append($posts.fadeIn(1000));
-  });
+	var container = $('.feed');
+    var nextPage = parseInt(container.attr("data-page")) + 1;
+    var totalPages = parseInt(container.attr("data-totalPages"));
+
+    container.attr("data-page", nextPage);
+    $.get("/page/" + nextPage, function(data) {
+        var htmlData = $.parseHTML(data);
+        var $new = $(htmlData).find(".feed-row").hide();
+        container.append($new.fadeIn(1000));
+    });
 }
